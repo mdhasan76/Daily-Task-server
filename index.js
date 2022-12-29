c32bf1f5af950e2defec1829296f5b09229c8b62
Md Hasan <mdhasan8064@gmail.com>
2022-12-29 19:07:32 +0600

    create some rest api

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) =>{
 
 
 //mongoDB setup
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.PRIVATE_KEY_MU}:${process.env.PRIVATE_KEY_MP}@cluster0.dof2kcq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -23,7 +23,54 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //mongoDB async start
 const run = async() =>{
 
-    
+    const taskCollection = client.db("Daily_Task").collection("All_Task");
+
+
+    app.get("/mytask", async(req, res) =>{
+        const email = req.query.email
+        const query = {email:email};
+        const result = await taskCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    app.post('/addtask', async (req, res) =>{
+        const data = req.body;
+        const result = await taskCollection.insertOne(data);
+        res.send(result);
+    });
+
+    app.delete("/delete/:id", async(req, res) =>{
+        const id = req.params.id;
+        const query = {id: ObjectId(id)};
+        const result = await taskCollection.deleteOne(query);
+        res.send(result)
+    });
+
+    app.patch("/complatetask/:id", async(req, res) =>{
+        const id = req.params.id;
+        const filter = {id: ObjectId(id)};
+        const option = {upsert: true};
+        const updateDoc = {
+            $set:{
+                isComplate: true
+            }
+        }
+        const result = await taskCollection.updateOne(filter, updateDoc, option);
+        res.send(result)
+    })
+
+
+    app.patch("/canceltask/:id", async(req, res) =>{
+        const id = req.params.id;
+        const filter = {id: ObjectId(id)};
+        const updateDoc = {
+            $set:{
+                isComplate: false
+            }
+        }
+        const result = await taskCollection.updateOne(filter, updateDoc, option);
+        res.send(result)
+    })
 
 
 }
